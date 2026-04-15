f200cc209e2608aeea6bc27f52783dd0d65f64ac
priyankakapoor <priyankakapoor1993@gmail.com>
2026-04-15 13:18:28 +0530

    initial comment

---
+++ demo1.sh
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My DevOps App</title>
+</head>
+<body>
+
+    <h2>Add User</h2>
+
+    <form action="insert.php" method="post">
+        Name: <input type="text" name="name" required>
+        <input type="submit" value="Add">
+    </form>
+
+    <br><br>
+
+    <a href="index.php">View Users</a>
+
+</body>
+</html>
